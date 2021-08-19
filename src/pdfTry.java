1532e44e92722e142c227d07dcb040d3147cc62d
Aayushjshah <2001aayushshah@gmail.com>
2021-08-19 21:43:16 +0100

    converted into maven project and pdf integrated sucessfully

--- src/pdfTry.java
+++ src/pdfTry.java
@@ -1,8 +1,10 @@
 public class pdfTry {
     pdfTry(){
         String[] values = {"Manraj","September","1991","Rina","4000","08-08-2021","Divyesh","Boi@Rina","1991","Rina","4000","08-08-2021","Divyesh","Boi@Rina"};
+        ReturnName rn = new ReturnName();
         createMonthReport cmr = new createMonthReport();
-        // cmr.setLayout(values);
+        System.out.print(rn.n());
+        
     }
     public static void main(String[] args){
         new pdfTry();
